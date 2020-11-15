24bd339e1f5b8236199be48de449c88e9e9e0321
Ibrahim Al Azhar <ibrahimalazhar264@gmail.com>
2020-11-15 21:31:50 +0600

    my first commit

---
+++ account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
+from .api import RegisterApi
+
+
+urlpatterns = [
+      path('api/register', RegisterApi.as_view()),
+]
+
